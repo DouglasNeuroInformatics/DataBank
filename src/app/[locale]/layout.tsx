dd695853ea4f33654a68aeab5db1114e4b9146a7
joshunrau <contact@joshuaunrau.com>
2023-05-24 11:11:46 -0400

    i18n works

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,21 +1,45 @@
-import { Sidebar } from '@/components/Sidebar';
-import { Inter } from 'next/font/google';
 import { clsx } from 'clsx';
+import { Inter } from 'next/font/google';
 import '@/styles/index.css';
+import { notFound } from 'next/navigation';
+import { NextIntlClientProvider } from 'next-intl';
+
+import { Sidebar } from '@/components/Sidebar';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export function generateStaticParams() {
+  return [{ locale: 'en' }, { locale: 'fr' }];
+}
 
 export const metadata = {
   title: 'Data Bank',
   description: 'Minimum Viable Product'
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({
+  children,
+  params: { locale }
+}: {
+  children: React.ReactNode;
+  params: {
+    locale: 'en' | 'fr';
+  };
+}) {
+  let messages;
+  try {
+    messages = (await import(`@/translations/${locale}.json`)).default;
+  } catch (error) {
+    notFound();
+  }
+
   return (
     <html lang="en">
       <body className={clsx(inter.className, 'flex')}>
-        <Sidebar />
-        <main>{children}</main>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <Sidebar />
+          <main>{children}</main>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
