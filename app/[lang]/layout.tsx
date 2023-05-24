8c68464ce71ff72a05799be40b321b23d34332a2
joshunrau <contact@joshuaunrau.com>
2023-05-24 13:30:03 -0400

    extract sidebar

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import { Inter } from 'next/font/google';
-import Image from 'next/image';
 
 import { clsx } from 'clsx';
 
+import { Sidebar } from '@/components/Sidebar';
 import { ClientTranslationsProvider } from '@/context/ClientTranslations';
 import { useServerTranslations } from '@/hooks/useServerTranslations';
 import { type Locale, i18n } from '@/i18n-config';
-import logo from '@/public/logo.png';
 import '@/styles/index.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -27,17 +26,7 @@ export default async function Root({ children, params }: { children: React.React
   return (
     <html lang={params.lang}>
       <body className={clsx(inter.className, 'flex')}>
-        <div className="flex h-screen w-80 flex-col bg-slate-900 p-3 text-slate-300">
-          <div className="flex items-center p-1 md:p-2">
-            <Image alt="logo" className="mr-2 w-14 md:w-16" src={logo} />
-            <span className="text-sm uppercase leading-tight antialiased md:text-base" style={{ maxWidth: '7.5em' }}>
-              Data Bank
-            </span>
-          </div>
-          <hr className="my-1" />
-          <nav></nav>
-          <hr className="my-1 mt-auto" />
-        </div>
+        <Sidebar />
         <main>
           <ClientTranslationsProvider t={t}>{children}</ClientTranslationsProvider>
         </main>
