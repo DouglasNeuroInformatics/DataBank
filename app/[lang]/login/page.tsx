55a0f3a59fe30d54485a9795843ac01ecaf63411
joshunrau <contact@joshuaunrau.com>
2023-05-25 00:00:37 -0400

    finally works

--- app/[lang]/login/page.tsx
+++ app/[lang]/login/page.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import { LoginForm } from '@/components/LoginForm';
 
 export default function LoginPage() {
-  return <LoginForm />
+  return <LoginForm />;
 }
--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,39 +1,52 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 
+import { Form } from '@douglasneuroinformatics/react-components';
 import { signIn } from 'next-auth/react';
 
-import { InputGroup } from './InputGroup';
+type LoginFormData = {
+  username: string;
+  password: string;
+};
 
 export const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
-    event.preventDefault();
-    void signIn('credentials', {
-      username,
-      password,
-      redirect: true,
-      callbackUrl: 'http://localhost:3000/en/home'
-    });
-  };
-
   return (
-    <div className="max-w-lg">
-      <h1 className="text-center font-medium text-2xl">Sign In</h1>
-      <form onSubmit={handleSubmit}>
-        <InputGroup label="Username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-        <InputGroup
-          label="Password"
-          name="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="bg-slate-700 w-full text-slate-50 p-2 rounded-lg">Login</button>
-      </form>
-    </div>
+    <Form<LoginFormData>
+      content={{
+        username: {
+          kind: 'text',
+          label: 'Username',
+          variant: 'short'
+        },
+        password: {
+          kind: 'text',
+          label: 'Password',
+          variant: 'password'
+        }
+      }}
+      validationSchema={{
+        type: 'object',
+        properties: {
+          username: {
+            type: 'string',
+            minLength: 1
+          },
+          password: {
+            type: 'string',
+            minLength: 1
+          }
+        },
+        required: ['username', 'password']
+      }}
+      onSubmit={(credentials) => {
+        void signIn('credentials', {
+          username: credentials.username,
+          password: credentials.password,
+          redirect: true,
+          callbackUrl: 'http://localhost:3000/en/home'
+        });
+      }}
+    />
   );
 };
