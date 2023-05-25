5b063c886e560d7d2da9c24c51ae1e4c673446ab
joshunrau <contact@joshuaunrau.com>
2023-05-25 09:57:44 -0400

    rm locale switcher

--- app/[lang]/home/page.tsx
+++ app/[lang]/home/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { Counter } from '@/components/Counter';
-import { LocaleSwitcher } from '@/components/LocaleSwitcher';
 import { useServerTranslations } from '@/hooks/useServerTranslations';
 import { Locale } from '@/i18n-config';
 
@@ -10,7 +9,6 @@ export default async function IndexPage({ params: { lang } }: { params: { lang: 
 
   return (
     <div>
-      <LocaleSwitcher />
       <p>Current locale: {lang}</p>
       <p>This text is rendered on the server: {t['server-component'].welcome}</p>
       <Counter />
