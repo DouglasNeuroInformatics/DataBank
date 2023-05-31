62c76bf283a6c737a325a709a81d64c9118cab5d
joshunrau <contact@joshuaunrau.com>
2023-05-31 19:20:46 -0400

    use nice form for login

--- src/app/[locale]/[auth]/login/page.tsx
+++ src/app/[locale]/[auth]/login/page.tsx
@@ -1,30 +1,56 @@
+'use client';
+
 import React from 'react';
 
-import { type Locale } from '@/lib/i18n';
-import { getTranslations } from '@/utils/get-translations';
+import { Form } from '@douglasneuroinformatics/react-components';
 
-interface LoginPageProps {
-  params: {
-    locale: Locale;
-  };
-}
+import { Branding } from '@/components/Branding';
+import { LanguageSwitcher } from '@/components/LanguageSwitcher';
+import { ThemeToggle } from '@/components/ThemeToggle';
+import { useClientTranslations } from '@/hooks/useClientTranslations';
 
-export default async function LoginPage({ params }: LoginPageProps) {
-  const t = await getTranslations(params.locale);
+type LoginCredentials = {
+  email: string;
+  password: string;
+};
+
+const CreateAccountPage = () => {
+  const t = useClientTranslations();
   return (
-    <div></div>
-    // <Form action={login}>
-    //   <Form.Header>
-    //     <Branding />
-    //     <h3 className="mt-2">{t.login}</h3>
-    //   </Form.Header>
-    //   <Form.TextField label={t.email} name="email" type="text" />
-    //   <Form.TextField label={t.password} name="password" type="password" />
-    //   <Form.SubmitButton label={t.login} />
-    //   <Form.Footer className="flex w-full justify-between">
-    //     <LanguageSwitcher dropdownDirection="up" />
-    //     <ThemeToggle />
-    //   </Form.Footer>
-    // </Form>
+    <div className="flex w-full flex-col gap-4 rounded-lg bg-white px-6 py-8 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800 md:p-8">
+      <div className="mb-3 flex flex-col items-center text-2xl font-semibold tracking-tight text-slate-900 dark:text-white">
+        <Branding />
+        <h3 className="mt-2">{t.login}</h3>
+      </div>
+      <Form<LoginCredentials>
+        content={{
+          email: { kind: 'text', label: t.email, variant: 'short' },
+          password: { kind: 'text', label: t.password, variant: 'password' }
+        }}
+        validationSchema={{
+          type: 'object',
+          properties: {
+            email: {
+              type: 'string',
+              minLength: 1
+            },
+            password: {
+              type: 'string',
+              minLength: 1
+            }
+          },
+          required: ['email', 'password']
+        }}
+        onSubmit={(data) => {
+          alert(JSON.stringify(data));
+        }}
+      />
+      <div className="flex w-full justify-between">
+        <LanguageSwitcher dropdownDirection="up" />
+        <ThemeToggle />
+      </div>
+    </div>
   );
-}
+};
+
+export default CreateAccountPage;
