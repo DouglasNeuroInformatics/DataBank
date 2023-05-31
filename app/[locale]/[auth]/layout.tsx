c0e1a071c6395c03a9f81f5c3f872e39b048493b
joshunrau <contact@joshuaunrau.com>
2023-05-31 09:51:06 -0400

    add lang to auth

--- app/[locale]/[auth]/layout.tsx
+++ app/[locale]/[auth]/layout.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import { LanguageSwitcher } from '@/components/LanguageSwitcher';
+
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => (
-  <div className="flex h-screen w-screen items-center justify-center">{children}</div>
+  <div className="flex h-screen w-screen flex-col items-center justify-center">
+    {children}
+    <div>
+      <LanguageSwitcher dropdownDirection="up" />
+    </div>
+  </div>
 );
 
 export default AuthLayout;
--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -1,7 +1,11 @@
+'use client';
+
 import React from 'react';
 
 import Link from 'next/link';
 
+import { clsx } from 'clsx';
+import { ArrowToggle } from '@douglasneuroinformatics/react-components';
 import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/24/solid';
 
@@ -10,7 +14,11 @@ import { Locale, i18n } from '@/lib/i18n';
 import { useRedirectedPathname } from '@/hooks/useRedirectedPathname';
 import { useLocale } from '@/hooks/useLocale';
 
-export const LanguageSwitcher = () => {
+interface LanguageSwitcherProps {
+  dropdownDirection?: 'up' | 'down';
+}
+
+export const LanguageSwitcher = ({ dropdownDirection }: LanguageSwitcherProps) => {
   const t = useClientTranslations();
   const locale = useLocale();
 
@@ -20,7 +28,11 @@ export const LanguageSwitcher = () => {
     <Menu as="div" className="relative">
       <Menu.Button className="flex items-center">
         <span className="uppercase">{locale}</span>
-        <ChevronDownIcon className="ml-1" height={16} width={16} />
+        <ChevronDownIcon
+          className={clsx('ml-1', { 'rotate-180': dropdownDirection === 'up' })}
+          height={16}
+          width={16}
+        />
       </Menu.Button>
       <Transition
         as={React.Fragment}
@@ -31,7 +43,7 @@ export const LanguageSwitcher = () => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 mt-2 flex min-w-full origin-top-right flex-col rounded-md border">
+        <Menu.Items className="absolute right-0 mt-2 flex min-w-full flex-col rounded-md border">
           {i18n.locales.map((locale) => (
             <Menu.Item key={locale}>
               <Link className="px-3 py-2 uppercase hover:bg-slate-100" href={redirectedPathName(locale)}>
