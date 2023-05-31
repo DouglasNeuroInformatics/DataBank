8a98dec2de76729799faaf1b2ca7e6b090ca022b
joshunrau <contact@joshuaunrau.com>
2023-05-30 23:30:15 -0400

    add dynamic toggle

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 
 import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
 
 export const ThemeToggle = () => {
+  const [theme, setTheme] = useState<'light' | 'dark'>();
+
   const handleToggle = () => {
     const root = document.querySelector('html');
     if (root) {
       const theme = root.classList.toggle('dark') ? 'dark' : 'light';
       document.cookie = `theme=${theme}`;
+      setTheme(theme);
     }
   };
 
   return (
     <button type="button" onClick={handleToggle}>
-      <MoonIcon height={24} width={24} />
-      {/* {theme === 'dark' ? <SunIcon height={24} width={24} /> : <MoonIcon height={24} width={24} />} */}
+      {theme === 'dark' ? <SunIcon height={24} width={24} /> : <MoonIcon height={24} width={24} />}
     </button>
   );
 };
