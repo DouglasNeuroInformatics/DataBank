65c958b6d7e43afa228f64b76872ea8006926905
joshunrau <contact@joshuaunrau.com>
2023-05-31 12:10:21 -0400

    use locale when routing to index

--- src/components/Branding.tsx
+++ src/components/Branding.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { useLocale } from '@/hooks/useLocale';
+
 export interface BrandingProps {
   /** The title to display next to the logo. If not defined, then no title will be displayed */
   title?: string;
@@ -10,8 +12,9 @@ export interface BrandingProps {
 
 /** Link to the landing page with the platform name and logo */
 export const Branding = ({ title }: BrandingProps) => {
+  const locale = useLocale();
   return (
-    <Link className="flex items-center" href="/">
+    <Link className="flex items-center" href={`/${locale}`}>
       <Image alt="logo" height="64" src="/logo.png" width="64" />
       {title && (
         <span className="whitespace-nowrap font-semibold lg:text-lg" style={{ maxWidth: '7.5em' }}>
