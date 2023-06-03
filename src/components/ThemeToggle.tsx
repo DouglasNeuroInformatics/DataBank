1c4bf6abfd8078b24c49d026e2819b6795838226
joshunrau <contact@joshuaunrau.com>
2023-06-03 12:27:36 -0400

    fix cookie

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 
 import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
+import Cookies from 'js-cookie';
 
 export const ThemeToggle = () => {
   const [theme, setTheme] = useState<'light' | 'dark'>();
@@ -11,13 +12,21 @@ export const ThemeToggle = () => {
     const root = document.querySelector('html');
     if (root) {
       const theme = root.classList.toggle('dark') ? 'dark' : 'light';
-      document.cookie = `theme=${theme}`;
+      Cookies.set('theme', theme, {
+        expires: 365,
+        path: '/',
+        sameSite: 'lax'
+      });
       setTheme(theme);
     }
   };
 
   return (
-    <button className="rounded-full p-2 transition-transform hover:bg-slate-200 dark:hover:bg-slate-700" type="button" onClick={handleToggle}>
+    <button
+      className="rounded-full p-2 transition-transform hover:bg-slate-200 dark:hover:bg-slate-700"
+      type="button"
+      onClick={handleToggle}
+    >
       {theme === 'dark' ? <SunIcon height={24} width={24} /> : <MoonIcon height={24} width={24} />}
     </button>
   );
