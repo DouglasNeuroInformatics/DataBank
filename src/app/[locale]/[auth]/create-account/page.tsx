c6cbea0e5c55c1e159c331ed3d4be51085e6edda
joshunrau <contact@joshuaunrau.com>
2023-06-06 11:15:39 -0400

    add react in scope

--- src/app/[locale]/[auth]/create-account/page.tsx
+++ src/app/[locale]/[auth]/create-account/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import React from 'react';
+
 import { Form } from '@douglasneuroinformatics/react-components';
 
 import { Branding } from '@/components/Branding';
--- src/app/[locale]/[auth]/login/page.tsx
+++ src/app/[locale]/[auth]/login/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import React from 'react';
+
 import { useRouter } from 'next/navigation';
 
 import { Form } from '@douglasneuroinformatics/react-components';
