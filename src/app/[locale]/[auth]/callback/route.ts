f9488118985b44e1b0be5ba059fb4c33366418a4
joshunrau <contact@joshuaunrau.com>
2023-06-12 14:34:38 -0400

    ise rec

--- src/app/[locale]/[auth]/callback/route.ts
+++ src/app/[locale]/[auth]/callback/route.ts
@@ -3,15 +3,15 @@ import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export async function GET(request: NextRequest) {
-  const requestUrl = new URL(request.url);
+export async function GET(req: NextRequest) {
+  const supabase = createRouteHandlerClient({ cookies });
+  const requestUrl = new URL(req.url);
   const code = requestUrl.searchParams.get('code');
 
   if (code) {
-    const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin);
+  return NextResponse.redirect(new URL('/', req.url));
 }
