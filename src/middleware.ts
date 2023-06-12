54e206b4edca1ecf69c93f73ce19264d2f5685f8
joshunrau <contact@joshuaunrau.com>
2023-06-12 13:39:18 -0400

    add auth redict

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,55 @@
+import { match as matchLocale } from '@formatjs/intl-localematcher';
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
-import { NextResponse } from 'next/server';
+import Negotiator from 'negotiator';
+import { type NextRequest, NextResponse } from 'next/server';
 
-import { withLocalization } from './middlewares/withLocalization';
+import { i18n } from '@/i18n';
 
-export const middleware = withLocalization(async (req) => {
+const STATIC_FILE_EXTENSIONS = ['jpg', 'png', 'svg'];
+
+function getLocale(req: NextRequest): string | undefined {
+  // Negotiator expects plain object so we need to transform headers
+  const negotiatorHeaders: Record<string, string> = {};
+  req.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+
+  // Use negotiator and intl-localematcher to get best locale
+  const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
+  const locales = [...i18n.locales];
+  return matchLocale(languages, locales, i18n.defaultLocale);
+}
+
+export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
+  const pathname = req.nextUrl.pathname;
+
+  // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
+  for (const extension of STATIC_FILE_EXTENSIONS) {
+    if (pathname.endsWith(`.${extension}`)) {
+      return;
+    }
+  }
+
+  // Check if there is any supported locale in the pathname
+  const pathnameIsMissingLocale = i18n.locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  );
+
+  // Redirect if there is no locale (e.g., /products -> /en/products)
+  if (pathnameIsMissingLocale) {
+    const locale = getLocale(req) || i18n.defaultLocale;
+    return NextResponse.redirect(new URL(`/${locale}/${pathname}`, req.url));
+  }
+
+  // AUTH
+
   const supabase = createMiddlewareClient({ req, res });
-  await supabase.auth.getSession();
-  return res;
-});
+
+  const userResponse = await supabase.auth.getUser();
+  console.log(userResponse);
+  
+}
 
 export const config = {
+  // Matcher ignoring `/_next/` and `/api/`
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
