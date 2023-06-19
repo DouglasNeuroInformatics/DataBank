7d59c23bb9f5ddc9249f7a5b4a7d00d6aaec7632
joshunrau <contact@joshuaunrau.com>
2023-06-19 19:26:28 -0400

    add locale

--- server/src/core/decorators/request-user.decorator.ts
+++ server/src/core/decorators/request-user.decorator.ts
@@ -4,9 +4,9 @@ import { CurrentUser } from '@databank/types';
 import { type Request } from 'express';
 
 export const RequestUser = createParamDecorator((key: keyof CurrentUser, context: ExecutionContext) => {
-  const request = context.switchToHttp().getRequest<Request & { user: CurrentUser }>();
+  const request = context.switchToHttp().getRequest<Request>();
   if (key) {
-    return request.user[key];
+    return request.user?.[key];
   }
   return request.user;
 });
