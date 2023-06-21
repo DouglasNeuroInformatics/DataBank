3404e18444852a76f6d8c990f144e9afd638045c
joshunrau <contact@joshuaunrau.com>
2023-06-21 11:20:05 -0400

    add transition

--- client/src/components/Layout/UserDropup.tsx
+++ client/src/components/Layout/UserDropup.tsx
@@ -1,6 +1,5 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 
-import { ArrowToggle, useOnClickOutside } from '@douglasneuroinformatics/react-components';
 import { Menu, Transition } from '@headlessui/react';
 import { UserCircleIcon } from '@heroicons/react/24/solid';
 import { useTranslation } from 'react-i18next';
@@ -16,18 +15,28 @@ export const UserDropup = () => {
       <Menu.Button>
         <UserCircleIcon className="h-8 w-8" />
       </Menu.Button>
-      <Menu.Items className="absolute bottom-12 left-0 whitespace-nowrap rounded-sm border shadow-lg dark:border-slate-700 dark:bg-slate-800">
-        <Menu.Item>
-          <button className="block p-2 first-letter:capitalize hover:bg-slate-700" onClick={() => auth.logout()}>
-            {t('logout')}
-          </button>
-        </Menu.Item>
-        <Menu.Item>
-          <Link className="block p-2 first-letter:capitalize hover:bg-slate-700" to="/user">
-            {t('preferences')}
-          </Link>
-        </Menu.Item>
-      </Menu.Items>
+      <Transition
+        as={React.Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute bottom-12 left-0 origin-bottom-left whitespace-nowrap rounded-sm border shadow-lg dark:border-slate-700 dark:bg-slate-800">
+          <Menu.Item>
+            <button className="block p-2 first-letter:capitalize hover:bg-slate-700" onClick={() => auth.logout()}>
+              {t('logout')}
+            </button>
+          </Menu.Item>
+          <Menu.Item>
+            <Link className="block p-2 first-letter:capitalize hover:bg-slate-700" to="/user">
+              {t('preferences')}
+            </Link>
+          </Menu.Item>
+        </Menu.Items>
+      </Transition>
     </Menu>
   );
 };
