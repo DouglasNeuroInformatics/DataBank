ffbbc4a564d21f9a7af1ce0e50dc475524715616
joshunrau <contact@joshuaunrau.com>
2023-06-23 18:45:38 -0400

    sstyling

--- client/src/features/shared/pages/SharedPage.tsx
+++ client/src/features/shared/pages/SharedPage.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import { DatasetInfo } from '@databank/types';
+import { SearchBar } from '@douglasneuroinformatics/react-components';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ import { Heading } from '@/components/Heading';
 export const SharedPage = () => {
   const navigate = useNavigate();
   const [availableDatasets, setAvailableDatasets] = useState<DatasetInfo[]>();
+  const [searchTerm, setSearchTerm] = useState('');
 
   const fetchAvailable = async () => {
     const response = await axios.get<DatasetInfo[]>('/v1/datasets/available');
@@ -21,20 +23,37 @@ export const SharedPage = () => {
     void fetchAvailable();
   }, []);
 
+  const filteredDatasets = useMemo(() => {
+    if (!availableDatasets) {
+      return null;
+    }
+    return availableDatasets.filter((dataset) => dataset.name.toUpperCase().includes(searchTerm.toUpperCase()));
+  }, [availableDatasets, searchTerm]);
+
   return (
     <div className="flex h-full w-full flex-col">
       <Heading title="Shared Datasets" />
+      <SearchBar
+        className="my-3 rounded-sm"
+        size="sm"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
       <div className="overflow-hidden">
-        {availableDatasets ? (
+        {filteredDatasets === null ? (
+          <SuspenseFallback />
+        ) : filteredDatasets.length === 0 ? (
+          <div>
+            <p>No Results Found</p>
+          </div>
+        ) : (
           <ul className="h-full overflow-x-visible overflow-y-scroll rounded-sm shadow">
-            {availableDatasets.map((dataset) => (
+            {filteredDatasets.map((dataset) => (
               <li key={dataset._id}>
                 <DatasetCard dataset={dataset} onClick={({ _id }) => navigate(_id)} />
               </li>
             ))}
           </ul>
-        ) : (
-          <SuspenseFallback />
         )}
       </div>
     </div>
