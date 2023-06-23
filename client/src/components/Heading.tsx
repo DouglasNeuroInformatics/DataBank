e11cc8e465ab2f60770bc3f8298b578b185ae796
joshunrau <contact@joshuaunrau.com>
2023-06-23 10:52:32 -0400

    fix mobile issue

--- client/src/components/Heading.tsx
+++ client/src/components/Heading.tsx
@@ -11,7 +11,7 @@ export const Heading = ({ title, subtitle, children, border = true }: HeadingPro
   return (
     <div
       className={twMerge(
-        'mb-3 gap-5 border-slate-300 py-5 dark:border-slate-600 sm:flex sm:items-center sm:justify-between',
+        'mb-3 flex flex-col gap-5 border-slate-300 py-5 dark:border-slate-600 sm:flex-row sm:items-center sm:justify-between',
         border && 'border-b'
       )}
     >
--- client/src/utils/withTransition.tsx
+++ client/src/utils/withTransition.tsx
@@ -5,6 +5,7 @@ export function withTransition<T extends object>(Component: React.ComponentType<
     return (
       <motion.div
         animate={{ opacity: 1 }}
+        className="h-full w-full"
         exit={{ opacity: 0 }}
         initial={{ opacity: 0 }}
         transition={{ duration: 0.15 }}
