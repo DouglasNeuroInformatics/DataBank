826ed383689c163283e061075220559fb0491823
joshunrau <contact@joshuaunrau.com>
2023-06-23 11:12:44 -0400

    add useAvailableDatasets

---
+++ client/src/hooks/useAvailableDatasets.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+import { DatasetInfo } from '@databank/types';
+import axios from 'axios';
+
+import { useAuthStore } from '@/stores/auth-store';
+
+/**
+ * Returns an array of info about available datasets
+ * @param options.onlyCurrentUser - if true, then only datasets owned by the current user will be returned
+ * @returns 
+ */
+export function useAvailableDatasets(options?: { onlyCurrentUser?: boolean }) {
+  const auth = useAuthStore();
+  const [availableDatasets, setAvailableDatasets] = useState<DatasetInfo[]>([]);
+
+  const fetchAvailable = async () => {
+    //const url = '/v1/datasets/available' + options?.onlyCurrentUser ? `?owner=${auth.currentUser.}`
+    const response = await axios.get<DatasetInfo[]>('/v1/datasets/available');
+    setAvailableDatasets(response.data);
+  };
+
+  useEffect(() => {
+    void fetchAvailable();
+  }, [auth.currentUser]);
+
+  return availableDatasets;
+}
