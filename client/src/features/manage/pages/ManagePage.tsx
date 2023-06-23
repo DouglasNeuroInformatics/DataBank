7b45844aabb6c69bc794339f38bba60014f636e4
joshunrau <contact@joshuaunrau.com>
2023-06-23 17:40:32 -0400

    add dropzone

--- client/src/features/manage/pages/ManagePage.tsx
+++ client/src/features/manage/pages/ManagePage.tsx
@@ -1,9 +1,13 @@
-import { Heading } from '@/components/Heading';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export const ManagePage = () => {
   return (
-    <div className="h-full w-full">
-      <Heading title="Your Datasets" />
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <MagnifyingGlassIcon className="text-slate-700 dark:text-slate-200" height={64} width={64} />
+        <h3 className="mt-2 font-medium text-slate-700 dark:text-slate-200">No Dataset Selected</h3>
+        <p className="text-sm text-slate-500">Please select a dataset, or add a new dataset using the button above</p>
+      </div>
     </div>
   );
 };
