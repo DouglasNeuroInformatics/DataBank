be3f324461319adffe154b8aa24608a6f689b362
joshunrau <contact@joshuaunrau.com>
2023-06-23 16:13:54 -0400

    add useDataset for manage

--- client/src/features/manage/pages/ManageDatasetPage.tsx
+++ client/src/features/manage/pages/ManageDatasetPage.tsx
@@ -1,9 +1,20 @@
-import { Heading } from '@/components/Heading';
+import { Table } from '@douglasneuroinformatics/react-components';
+import { useParams } from 'react-router-dom';
+
+import { SuspenseFallback } from '@/components';
+import { useDataset } from '@/hooks/useDataset';
 
 export const ManageDatasetPage = () => {
-  return (
-    <div>
-      <Heading title="Manage Datasets" />
+  const params = useParams();
+  const { dataset, table } = useDataset(params.id!);
+
+  return dataset ? (
+    <div className="flex h-full w-full flex-col">
+      <div className="flex-grow overflow-hidden">
+        <Table columns={table.columns} data={table.data} />
+      </div>
     </div>
+  ) : (
+    <SuspenseFallback />
   );
 };
