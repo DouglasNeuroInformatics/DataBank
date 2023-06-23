b464b011cf4d58a296bf065db9937deefee08654
joshunrau <contact@joshuaunrau.com>
2023-06-23 19:39:39 -0400

    update layout

--- client/src/features/manage/pages/ManagePage.tsx
+++ client/src/features/manage/pages/ManagePage.tsx
@@ -6,7 +6,6 @@ export const ManagePage = () => {
       <div className="flex flex-col items-center justify-center">
         <MagnifyingGlassIcon className="text-slate-700 dark:text-slate-200" height={64} width={64} />
         <h3 className="mt-2 font-medium text-slate-700 dark:text-slate-200">No Dataset Selected</h3>
-        <p className="text-sm text-slate-500">Please select a dataset, or add a new dataset using the button above</p>
       </div>
     </div>
   );
