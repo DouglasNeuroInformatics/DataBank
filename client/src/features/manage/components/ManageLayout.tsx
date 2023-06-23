a39de20882b240c43a4509ef165e03821d130b66
joshunrau <contact@joshuaunrau.com>
2023-06-23 16:59:26 -0400

    search works

--- client/src/features/manage/components/ManageLayout.tsx
+++ client/src/features/manage/components/ManageLayout.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from 'react';
 
 import { DatasetInfo } from '@databank/types';
-import { SearchBar } from '@douglasneuroinformatics/react-components';
+import { Button, SearchBar } from '@douglasneuroinformatics/react-components';
 import axios from 'axios';
 import { Outlet, useNavigate } from 'react-router-dom';
 
@@ -13,6 +13,7 @@ export const ManageLayout = () => {
   const navigate = useNavigate();
   const { currentUser } = useAuthStore();
   const [availableDatasets, setAvailableDatasets] = useState<DatasetInfo[]>([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     axios
@@ -22,21 +23,17 @@ export const ManageLayout = () => {
   }, [currentUser]);
 
   const filteredDatasets = useMemo(() => {
-    return availableDatasets;
-  }, [availableDatasets]);
+    return availableDatasets.filter((dataset) => dataset.name.toUpperCase().includes(searchTerm.toUpperCase()));
+  }, [availableDatasets, searchTerm]);
 
   return (
     <div className="flex h-full w-full flex-col">
-      <Heading title="Manage Datasets" />
+      <Heading title="Manage Datasets">
+        <Button label="Add New" size="sm" />
+      </Heading>
       <div className="flex h-full gap-5 overflow-hidden">
-        <div className="h-full w-1/3 border border-dashed 2xl:w-1/4">
-          <div className="my-2">
-            <h3 className="text-lg font-medium">Datasets</h3>
-            <p className="text-sm text-slate-600 dark:text-slate-300">
-              Search total of {availableDatasets.length} datasets
-            </p>
-          </div>
-          <SearchBar />
+        <div className="h-full w-1/3 2xl:w-1/4">
+          <SearchBar className="my-3" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
           <ul className="h-full divide-y divide-slate-300 overflow-scroll" role="list">
             {filteredDatasets.map((dataset) => (
               <li key={dataset._id}>
@@ -45,7 +42,7 @@ export const ManageLayout = () => {
             ))}
           </ul>
         </div>
-        <div className="w-2/3 border border-dashed p-2 2xl:w-3/4">
+        <div className="w-2/3 2xl:w-3/4">
           <Outlet />
         </div>
       </div>
