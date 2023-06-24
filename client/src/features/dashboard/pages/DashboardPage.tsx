e4e40420113bed5fe64e4b3405a53464639a4815
joshunrau <contact@joshuaunrau.com>
2023-06-24 12:35:31 -0400

    add feed

--- client/src/features/dashboard/pages/DashboardPage.tsx
+++ client/src/features/dashboard/pages/DashboardPage.tsx
@@ -1,9 +1,14 @@
+import { Feed } from '../components/Feed';
+
 import { Heading } from '@/components/Heading';
 
 export const DashboardPage = () => {
   return (
     <div>
       <Heading title="Dashboard" />
+      <div className="mx-auto mt-10 max-w-xl">
+        <Feed />
+      </div>
     </div>
   );
 };
