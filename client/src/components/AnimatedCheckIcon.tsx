03184b13008afb5489c9d83e5b0907a43f89acfa
joshunrau <contact@joshuaunrau.com>
2023-06-25 09:17:37 -0400

    add animated check

---
+++ client/src/components/AnimatedCheckIcon.tsx
@@ -0,0 +1,31 @@
+import { motion } from 'framer-motion';
+
+export interface AnimatedCheckIconProps {
+  className: string;
+  onComplete?: () => void;
+}
+
+export const AnimatedCheckIcon = ({ className, onComplete }: AnimatedCheckIconProps) => (
+  <svg
+    className={className}
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={1.5}
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <motion.path
+      animate={{ pathLength: 1 }}
+      d="M4.5 12.75l6 6 9-13.5"
+      initial={{ pathLength: 0 }}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      transition={{
+        type: 'tween',
+        duration: 0.3,
+        ease: 'easeIn'
+      }}
+      onAnimationComplete={onComplete}
+    />
+  </svg>
+);
