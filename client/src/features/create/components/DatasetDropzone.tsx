271559802167bec7deac2d6bf907019e17e19a08
joshunrau <contact@joshuaunrau.com>
2023-06-25 09:17:37 -0400

    add animated check

--- client/src/features/create/components/DatasetDropzone.tsx
+++ client/src/features/create/components/DatasetDropzone.tsx
@@ -2,19 +2,20 @@ import { useCallback, useState } from 'react';
 
 import { Button, useNotificationsStore } from '@douglasneuroinformatics/react-components';
 import { CloudArrowUpIcon } from '@heroicons/react/24/outline';
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { FileRejection, useDropzone } from 'react-dropzone';
 import { useTranslation } from 'react-i18next';
 
+import { AnimatedCheckIcon } from '@/components/AnimatedCheckIcon';
 import { type ParsedCSV, parseCSV } from '@/utils/parse-csv';
 
 export interface DatasetDropzoneProps {
   onSubmit: ({ data, fields }: ParsedCSV) => void;
 }
 
-export const DatasetDropzone = () => {
+export const DatasetDropzone = ({ onSubmit }: DatasetDropzoneProps) => {
   const [file, setFile] = useState<File | null>(null);
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [result, setResult] = useState<ParsedCSV | null>(null);
   const notifications = useNotificationsStore();
   const { t } = useTranslation();
 
@@ -26,7 +27,7 @@ export const DatasetDropzone = () => {
     setFile(acceptedFiles[0]);
   }, []);
 
-  const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: {
       'text/csv': ['.csv'],
       'text/plain': ['.csv', '.tsv']
@@ -37,18 +38,16 @@ export const DatasetDropzone = () => {
 
   const handleSubmit = async (file: File) => {
     try {
-      const results = await parseCSV(file);
-      notifications.addNotification({ type: 'success' });
-      console.log(results);
-      setIsSubmitted(true);
+      const result = await parseCSV(file);
+      setResult(result);
     } catch (error) {
+      console.error(error);
+      setFile(null);
       if (error instanceof Error) {
         notifications.addNotification({ type: 'error', message: error.message });
       } else {
         notifications.addNotification({ type: 'error', message: t('unexpectedError') });
       }
-      console.error(error);
-      setFile(null);
     }
   };
 
@@ -58,19 +57,34 @@ export const DatasetDropzone = () => {
         className="h-64 cursor-pointer rounded-lg border-2 border-dashed border-slate-300 p-6 text-slate-600 dark:border-slate-600 dark:text-slate-300"
         {...getRootProps()}
       >
-        <AnimatePresence>
-          {isSubmitted ? (
-            <div>
-              <h1>Yay!</h1>
-            </div>
+        <AnimatePresence mode="wait">
+          {result ? (
+            <motion.div
+              animate={{ opacity: 1, y: 0 }}
+              className="flex h-full items-center justify-center"
+              initial={{ opacity: 0, y: -10 }}
+              key={0}
+            >
+              <AnimatedCheckIcon
+                className="h-12 w-12"
+                onComplete={() => {
+                  onSubmit(result);
+                }}
+              />
+            </motion.div>
           ) : (
-            <div className="flex h-full flex-col items-center justify-center" key={1}>
+            <motion.div
+              animate={{ opacity: 1, y: 0 }}
+              className="flex h-full flex-col items-center justify-center"
+              exit={{ opacity: 0, y: 10 }}
+              key={1}
+            >
               <CloudArrowUpIcon height={40} width={40} />
               <p className="mt-1 text-center text-sm">
                 {file ? file.name : isDragActive ? t('releaseToUpload') : t('dropHere')}
               </p>
               <input {...getInputProps()} />
-            </div>
+            </motion.div>
           )}
         </AnimatePresence>
       </div>
@@ -88,7 +102,10 @@ export const DatasetDropzone = () => {
           label={t('reset')}
           type="button"
           variant="secondary"
-          onClick={() => setFile(null)}
+          onClick={() => {
+            setFile(null);
+            setResult(null);
+          }}
         />
       </div>
     </div>
