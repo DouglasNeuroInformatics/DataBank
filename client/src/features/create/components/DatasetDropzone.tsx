3dea148b5f39444cecb69ed217bafb1c98d8ad21
joshunrau <contact@joshuaunrau.com>
2023-06-25 09:00:22 -0400

    add reset

--- client/src/features/create/components/DatasetDropzone.tsx
+++ client/src/features/create/components/DatasetDropzone.tsx
@@ -2,6 +2,7 @@ import { useCallback, useState } from 'react';
 
 import { Button, useNotificationsStore } from '@douglasneuroinformatics/react-components';
 import { CloudArrowUpIcon } from '@heroicons/react/24/outline';
+import { AnimatePresence } from 'framer-motion';
 import { FileRejection, useDropzone } from 'react-dropzone';
 import { useTranslation } from 'react-i18next';
 
@@ -13,6 +14,7 @@ export interface DatasetDropzoneProps {
 
 export const DatasetDropzone = () => {
   const [file, setFile] = useState<File | null>(null);
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const notifications = useNotificationsStore();
   const { t } = useTranslation();
 
@@ -38,6 +40,7 @@ export const DatasetDropzone = () => {
       const results = await parseCSV(file);
       notifications.addNotification({ type: 'success' });
       console.log(results);
+      setIsSubmitted(true);
     } catch (error) {
       if (error instanceof Error) {
         notifications.addNotification({ type: 'error', message: error.message });
@@ -52,20 +55,42 @@ export const DatasetDropzone = () => {
   return (
     <div className="w-full sm:max-w-md">
       <div
-        className="flex h-64 cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-slate-300 p-6 text-slate-600 dark:border-slate-600 dark:text-slate-300"
+        className="h-64 cursor-pointer rounded-lg border-2 border-dashed border-slate-300 p-6 text-slate-600 dark:border-slate-600 dark:text-slate-300"
         {...getRootProps()}
       >
-        <CloudArrowUpIcon height={40} width={40} />
-        <p className="text-center text-sm mt-1">{file ? file.name : isDragActive ? t('releaseToUpload') : t('dropHere')}</p>
-        <input {...getInputProps()} />
+        <AnimatePresence>
+          {isSubmitted ? (
+            <div>
+              <h1>Yay!</h1>
+            </div>
+          ) : (
+            <div className="flex h-full flex-col items-center justify-center" key={1}>
+              <CloudArrowUpIcon height={40} width={40} />
+              <p className="mt-1 text-center text-sm">
+                {file ? file.name : isDragActive ? t('releaseToUpload') : t('dropHere')}
+              </p>
+              <input {...getInputProps()} />
+            </div>
+          )}
+        </AnimatePresence>
       </div>
-      <Button
-        className="mt-2 w-full"
-        disabled={!file}
-        label={t('submit')}
-        type="button"
-        onClick={() => handleSubmit(file!)}
-      />
+      <div className="flex gap-2">
+        <Button
+          className="mt-2 w-full"
+          disabled={!file}
+          label={t('submit')}
+          type="button"
+          onClick={() => handleSubmit(file!)}
+        />
+        <Button
+          className="mt-2 w-full"
+          disabled={!file}
+          label={t('reset')}
+          type="button"
+          variant="secondary"
+          onClick={() => setFile(null)}
+        />
+      </div>
     </div>
   );
 };
