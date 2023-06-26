c737dce7dbdfaec6d0ce0b731d6652780a27b42a
joshunrau <contact@joshuaunrau.com>
2023-06-26 00:50:07 -0400

    fix hook

--- client/src/hooks/useDataset.ts
+++ client/src/hooks/useDataset.ts
@@ -1,23 +1,25 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 
-import { TDataset } from '@databank/types';
-import { TableColumn, TableEntry, useDownload } from '@douglasneuroinformatics/react-components';
+import { DatasetEntry, TDataset } from '@databank/types';
+import { TableColumn, useDownload } from '@douglasneuroinformatics/react-components';
 import axios from 'axios';
 import { unparse } from 'papaparse';
-import { Primitive } from 'type-fest';
 
-export type TableData<T extends TDataset, TEntry extends TableEntry = { [K in keyof T['columns']]: unknown }> = {
-  columns: TableColumn<TEntry>[];
-  data: Record<string, Primitive>[];
+export type TableData<T extends DatasetEntry> = {
+  columns: TableColumn<T>[];
+  data: T[];
 };
 
-export function useDataset<T extends TDataset = TDataset>(id: string) {
+export function useDataset<
+  TEntry extends DatasetEntry = DatasetEntry,
+  TData extends TDataset<TEntry> = TDataset<TEntry>
+>(id: string) {
   const dl = useDownload();
-  const [dataset, setDataset] = useState<T | null>(null);
+  const [dataset, setDataset] = useState<TData | null>(null);
 
   useEffect(() => {
     axios
-      .get<T>(`/v1/datasets/${id}`)
+      .get<TData>(`/v1/datasets/${id}`)
       .then((response) => setDataset(response.data))
       .catch(console.error);
   }, [id]);
@@ -27,11 +29,11 @@ export function useDataset<T extends TDataset = TDataset>(id: string) {
       if (!dataset) {
         throw new Error(`Expected dataset with ID '${id}' is undefined`);
       }
-      const cols = Object.keys(dataset.columns);
-      const matrix: any[][] = [cols];
-      for (const item of Object.values(dataset.data)) {
+      const columnNames = dataset.columns.map(({ name }) => name);
+      const matrix: any[][] = [columnNames];
+      for (const item of dataset.data) {
         const row: any[] = [];
-        for (const col of cols) {
+        for (const col of columnNames) {
           row.push(item[col]);
         }
         matrix.push(row);
@@ -44,13 +46,13 @@ export function useDataset<T extends TDataset = TDataset>(id: string) {
     [dl, dataset]
   );
 
-  const table: TableData<T> | null = useMemo(() => {
+  const table: TableData<TEntry> | null = useMemo(() => {
     if (!dataset) {
       return null;
     }
     return {
-      columns: Object.keys(dataset.columns).map((col) => ({ label: col, field: col })),
-      data: Object.values(dataset.data)
+      columns: dataset.columns.map((column) => ({ label: column.name, field: column.name })),
+      data: dataset.data
     };
   }, [dataset]);
 
