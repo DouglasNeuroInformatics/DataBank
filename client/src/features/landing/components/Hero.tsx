070554bff69d2614cfde3e385b2688dafd3cf4ca
joshunrau <contact@joshuaunrau.com>
2023-06-27 09:03:17 -0400

    adjust hero

--- client/src/features/landing/components/Hero.tsx
+++ client/src/features/landing/components/Hero.tsx
@@ -12,24 +12,23 @@ export const Hero = () => {
     <section className="flex gap-5 text-center xl:text-left">
       <motion.div
         animate={{ opacity: 1, y: 0 }}
-        className="flex-grow"
+        className="flex flex-grow flex-col justify-center"
         initial={{ opacity: 0, y: 10 }}
         transition={{ duration: 0.7 }}
       >
         <h1 className="text-3xl font-bold tracking-tight text-slate-900 dark:text-slate-100 md:text-4xl lg:text-5xl">
           {t('platformName')}
         </h1>
-        <p className="mx-auto mt-3 w-11/12 xl:mx-0 text-base text-slate-600 dark:text-slate-300 md:text-lg md:mt-5">
+        <p className="mx-auto mt-3 w-11/12 text-base text-slate-600 dark:text-slate-300 md:mt-5 md:text-lg xl:mx-0">
           {t('platformDescription')}
         </p>
-        <div className="mt-5 flex justify-center xl:justify-start gap-3">
+        <div className="mt-5 flex justify-center gap-3 xl:justify-start">
           <Button
-            className="text-sm md:text-base"
             label={t('getStarted')}
             type="button"
             onClick={() => navigate('/auth/create-account')}
           />
-          <Button className="text-sm md:text-base" label={t('learnMore')} type="button" variant="secondary" />
+          <Button label={t('learnMore')} type="button" variant="secondary" />
         </div>
       </motion.div>
       <motion.div
