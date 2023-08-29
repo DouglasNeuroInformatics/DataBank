a944e33948bfa9001fd2f9c31d8e4142c31e73ca
joshunrau <contact@joshuaunrau.com>
2023-08-29 13:25:19 -0400

    fix env types

--- web/src/typings/vite-env.d.ts
+++ web/src/typings/vite-env.d.ts
@@ -1,12 +1,13 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
 /// <reference types="vite/client" />
 
-type ImportMetaEnv = {
+interface ImportMetaEnv {
   readonly VITE_API_HOST: string;
-  readonly VITE_DEV_EMAIL: string;
-  readonly VITE_DEV_PASSWORD: string;
-  readonly VITE_DEV_BYPASS_AUTH: 'true' | 'false' | string;
-}
+  readonly VITE_DEV_EMAIL?: string;
+  readonly VITE_DEV_PASSWORD?: string;
+  readonly VITE_DEV_BYPASS_AUTH?: string;
+};
 
-type ImportMeta = {
+interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
