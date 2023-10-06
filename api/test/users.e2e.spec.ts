ba7dae6167e347d38da772c7ee5d4d96d235d5cd
joshunrau <contact@joshuaunrau.com>
2023-10-06 11:37:17 -0400

    add users e2e test

---
+++ api/test/users.e2e.spec.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { afterAll, beforeAll, describe, expect, it } from 'bun:test';
+
+import { HttpStatus } from '@nestjs/common';
+import { type NestExpressApplication } from '@nestjs/platform-express';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+
+import { AppModule } from '@/app.module';
+import { AuthGuard } from '@/auth/auth.guard';
+
+describe('/app', () => {
+  let app: NestExpressApplication;
+  let server: unknown;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule]
+    })
+      .overrideGuard(AuthGuard)
+      .useValue(undefined)
+      .compile();
+
+    app = moduleRef.createNestApplication({
+      logger: false
+    });
+
+    await app.init();
+    server = app.getHttpServer();
+  });
+
+  it('should be defined', () => {
+    expect(app).toBeDefined();
+  });
+
+  // describe('/users', () => {
+  //   describe('POST /users', () => {
+  //     it('should return status code 400 if an empty body is sent', async () => {
+  //       const response = await request(server).post('/users').send();
+  //       expect(response.status).toBe(HttpStatus.BAD_REQUEST);
+  //     });
+  //   });
+  // });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
