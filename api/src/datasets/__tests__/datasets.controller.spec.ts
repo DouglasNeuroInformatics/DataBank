58f64442dea9e25ade21bab431f2b8e6a6e74cdf
joshunrau <contact@joshuaunrau.com>
2023-10-10 12:19:17 -0400

    add unit tests

---
+++ api/src/datasets/__tests__/datasets.controller.spec.ts
@@ -0,0 +1,31 @@
+import { beforeEach, describe, expect, it } from 'bun:test';
+
+import { type MockedInstance, createMock } from '@douglasneuroinformatics/nestjs/testing';
+import { Test } from '@nestjs/testing';
+
+import { DatasetsController } from '../datasets.controller';
+import { DatasetsService } from '../datasets.service';
+
+describe('DatasetsController', () => {
+  let datasetsController: DatasetsController;
+  let datasetsService: MockedInstance<DatasetsService>;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [DatasetsController],
+      providers: [
+        {
+          provide: DatasetsService,
+          useValue: createMock(DatasetsService)
+        }
+      ]
+    }).compile();
+    datasetsController = moduleRef.get(DatasetsController);
+    datasetsService = moduleRef.get(DatasetsService);
+  });
+
+  it('should be defined', () => {
+    expect(datasetsController).toBeDefined();
+    expect(datasetsService).toBeDefined();
+  });
+});
