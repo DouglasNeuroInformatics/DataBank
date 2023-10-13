116d2b1995fda945e7190b511b9d323d4bdf1c4a
RyanHaniff <haniffryan.hr@gmail.com>
2023-10-13 13:00:58 -0400

    Created the describe() for AuthService

---
+++ api/src/auth/__tests__/auth.service.spec.ts
@@ -0,0 +1,43 @@
+describe('AuthService', () => {
+  let authService: AuthService;
+  let usersService: MockedInstance<UsersService>;
+  let cryptoService: MockedInstance<CryptoService>;
+  let jwtService: MockedInstance<JwtService>;
+
+  // called before every test to intialize the AuthService
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: ConfigService,
+          useValue: {
+            getOrThrow: (propertyPath: string) => propertyPath
+          }
+        },
+        {
+          provide: CryptoService,
+          useValue: createMock(CryptoService)
+        },
+        {
+          provide: I18nService,
+          useValue: createMock(I18nService)
+        },
+        { provide: JwtService, useValue: createMock(JwtService) },
+        {
+          provide: MailService,
+          useValue: createMock(MailService)
+        },
+        {
+          provide: UsersService,
+          useValue: createMock(UsersService)
+        }
+      ]
+    }).compile();
+    // get all the services we need from the moduleRef
+    authService = moduleRef.get(AuthService);
+    usersService = moduleRef.get(UsersService);
+    cryptoService = moduleRef.get(CryptoService);
+    jwtService = moduleRef.get(JwtService);
+  });
+  });
