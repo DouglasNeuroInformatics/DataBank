95ca9a63fac3282533e1a4c7343fccea8036c484
Weijie Tan <twj3454@hotmail.com>
2024-02-19 15:11:11 -0500

    commented forFeature, it is not used

--- api/src/prisma/prisma.module.ts
+++ api/src/prisma/prisma.module.ts
@@ -1,35 +1,35 @@
 import { type DynamicModule, Module } from '@nestjs/common';
-import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-import { PRISMA_CLIENT_TOKEN, getModelToken } from './prisma.utils';
+import { PRISMA_CLIENT_TOKEN } from './prisma.utils';
 
 @Module({})
 export class PrismaModule {
-    static forFeature<T extends Prisma.ModelName>(modelName: T): DynamicModule {
-        const modelToken =getModelToken(modelName)
-        return {
-          exports: [modelToken],
-          module: PrismaModule,
-          providers: [
-            {
-              inject: [PRISMA_CLIENT_TOKEN],
-              provide: modelToken,
-              useFactory: (client: PrismaClient) => {
-                return client[modelName.toLowerCase() as Lowercase<T>];
-              }
-            }
-          ]
-        };
-      }
-    static forRoot(): DynamicModule {
-        return {
-            exports: [PRISMA_CLIENT_TOKEN],
-            global: true,
-            module: PrismaModule,
-            providers: [{
-                provide: PRISMA_CLIENT_TOKEN,
-                useValue: new PrismaClient()
-            }]
-        }
+  // static forFeature<T extends Prisma.ModelName>(modelName: T): DynamicModule {
+  //     const modelToken =getModelToken(modelName)
+  //     return {
+  //       exports: [modelToken],
+  //       module: PrismaModule,
+  //       providers: [
+  //         {
+  //           inject: [PRISMA_CLIENT_TOKEN],
+  //           provide: modelToken,
+  //           useFactory: (client: PrismaClient) => {
+  //             return client[modelName.toLowerCase() as Lowercase<T>];
+  //           }
+  //         }
+  //       ]
+  //     };
+  //   }
+  static forRoot(): DynamicModule {
+    return {
+      exports: [PRISMA_CLIENT_TOKEN],
+      global: true,
+      module: PrismaModule,
+      providers: [{
+        provide: PRISMA_CLIENT_TOKEN,
+        useValue: new PrismaClient()
+      }]
     }
+  }
 }
