c2e814fb15e2d81d3d9e79a933ff5e04d546a172
Weijie Tan <twj3454@hotmail.com>
2024-03-07 10:22:39 -0500

    remove export for non-external z object

--- api/src/datasets/zod/dataset.ts
+++ api/src/datasets/zod/dataset.ts
@@ -2,11 +2,11 @@ import { DatasetLicense, PermissionLevel } from '@prisma/client';
 import { z } from 'zod';
 
 // ---------------------- Column Summaries ---------------------
-export const $BaseSummary = z.object({
+const $BaseSummary = z.object({
   count: z.number().int()
 });
 
-export const $IntSummary = $BaseSummary.extend({
+const $IntSummary = $BaseSummary.extend({
   max: z.number().int(),
   mean: z.number(),
   median: z.number(),
@@ -15,7 +15,7 @@ export const $IntSummary = $BaseSummary.extend({
   std: z.number()
 });
 
-export const $FloatSummary = $BaseSummary.extend({
+const $FloatSummary = $BaseSummary.extend({
   max: z.number(),
   mean: z.number(),
   median: z.number(),
@@ -23,42 +23,42 @@ export const $FloatSummary = $BaseSummary.extend({
   std: z.number()
 });
 
-export const $EnumSummary = $BaseSummary.extend({
+const $EnumSummary = $BaseSummary.extend({
   distribution: z.record(z.number().int())
 });
 
-export const $DatetimeSummary = $BaseSummary.extend({
+const $DatetimeSummary = $BaseSummary.extend({
   max: z.date(),
   min: z.date()
 });
 
 // ----------------------- Column Validation -----------------------
-export const $StringColumnValidation = z.object({
-  max: z.number().int(),
-  mean: z.number(),
-  median: z.number(),
-  min: z.number().int(),
-  mode: z.number().int(),
-  std: z.number()
-});
+// const $StringColumnValidation = z.object({
+//   max: z.number().int(),
+//   mean: z.number(),
+//   median: z.number(),
+//   min: z.number().int(),
+//   mode: z.number().int(),
+//   std: z.number()
+// });
 
-export const $NumericColumnValidation = z.object({
-  max: z.number().int(),
-  min: z.number().int()
-});
+// const $NumericColumnValidation = z.object({
+//   max: z.number().int(),
+//   min: z.number().int()
+// });
 
-export const $EnumColumnValidation = z.object({
-  allowedValues: z.string().array()
-});
+// export const $EnumColumnValidation = z.object({
+//   allowedValues: z.string().array()
+// });
 
-export const $DatetimeValidation = z.object({
-  max: z.date(),
-  min: z.date()
-  // pass ISO
-});
+// export const $DatetimeValidation = z.object({
+//   max: z.date(),
+//   min: z.date()
+//   // pass ISO
+// });
 
 // ---------------------------------------------------
-export const $TabularColumnInfo = z.object({
+const $TabularColumnInfo = z.object({
   dataPermission: z.nativeEnum(PermissionLevel),
   description: z.string().optional(),
   id: z.string(),
@@ -68,41 +68,41 @@ export const $TabularColumnInfo = z.object({
   tabularDataId: z.string()
 });
 
-export const $StringColumn = $TabularColumnInfo.extend({
+const $StringColumn = $TabularColumnInfo.extend({
   columnType: z.literal('STRING'),
   stringData: z.string().array(),
   summary: $BaseSummary
 });
 
-export type StringColumn = z.infer<typeof $StringColumn>;
+// type StringColumn = z.infer<typeof $StringColumn>;
 
-export const $IntColumn = $TabularColumnInfo.extend({
+const $IntColumn = $TabularColumnInfo.extend({
   columnType: z.literal('INT'),
   stringData: z.string().array(),
   summary: $IntSummary
 });
-export type IntColumn = z.infer<typeof $IntColumn>;
+// export type IntColumn = z.infer<typeof $IntColumn>;
 
-export const $FloatColumn = $TabularColumnInfo.extend({
+const $FloatColumn = $TabularColumnInfo.extend({
   columnType: z.literal('FLOAT'),
   stringData: z.string().array(),
   summary: $FloatSummary
 });
-export type FloatColumn = z.infer<typeof $FloatColumn>;
+// export type FloatColumn = z.infer<typeof $FloatColumn>;
 
-export const $EnumColumn = $TabularColumnInfo.extend({
+const $EnumColumn = $TabularColumnInfo.extend({
   columnType: z.literal('ENUM'),
   enumData: z.string().array(),
   summary: $EnumSummary
 });
-export type EnumColumn = z.infer<typeof $EnumColumn>;
+// export type EnumColumn = z.infer<typeof $EnumColumn>;
 
 export const $DatetimeColumn = $TabularColumnInfo.extend({
   columnType: z.literal('DATETIME'),
   datetimeData: z.date().array(),
   summary: $DatetimeSummary
 });
-export type DatetimeColumn = z.infer<typeof $DatetimeColumn>;
+// export type DatetimeColumn = z.infer<typeof $DatetimeColumn>;
 
 export const $TabularColumn = z.discriminatedUnion('columnType', [
   $StringColumn,
@@ -111,10 +111,10 @@ export const $TabularColumn = z.discriminatedUnion('columnType', [
   $EnumColumn,
   $DatetimeColumn
 ]);
-export type TabularColumn = z.infer<typeof $TabularColumn>;
+// export type TabularColumn = z.infer<typeof $TabularColumn>;
 
 // ------------------ Dataset ----------------------
-export const $DatasetInfo = z.object({
+const $DatasetInfo = z.object({
   createdAt: z.coerce.date(),
   description: z.string().optional(),
   id: z.string(),
@@ -123,28 +123,28 @@ export const $DatasetInfo = z.object({
   name: z.string(),
   updatedAt: z.coerce.date()
 });
-export type DatasetInfo = z.infer<typeof $DatasetInfo>;
+// export type DatasetInfo = z.infer<typeof $DatasetInfo>;
 
-export const $BaseDatasetModel = $DatasetInfo.extend({
+const $BaseDatasetModel = $DatasetInfo.extend({
   datasetType: z.literal('BASE')
 });
 
-export const $TabularData = z.object({
+const $TabularData = z.object({
   columns: z.array($TabularColumn),
   datasetId: z.string(),
   id: z.string(),
   primaryKeys: z.string().array()
 });
 
-export const $TabularDatasetModel = $DatasetInfo.extend({
+const $TabularDatasetModel = $DatasetInfo.extend({
   datasetType: z.literal('TABULAR'),
   tabularData: $TabularData
 });
 
-export const $DatasetModel = z.discriminatedUnion('datasetType', [$BaseDatasetModel, $TabularDatasetModel]);
+const $DatasetModel = z.discriminatedUnion('datasetType', [$BaseDatasetModel, $TabularDatasetModel]);
 export type DatasetModel = z.infer<typeof $DatasetModel>;
 // --------------- DTO --------------------------
-export const $CreateTabularDatasetDto = $DatasetInfo
+const $CreateTabularDatasetDto = $DatasetInfo
   .omit({
     createdAt: true,
     id: true,
