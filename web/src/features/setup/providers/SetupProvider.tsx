c2ffa31770c9e08a989e2089a34de030d0dbb8fd
Weijie Tan <twj3454@hotmail.com>
2024-05-23 12:37:13 -0400

    ui: setup feature init

--- web/src/features/setup/providers/SetupProvider.tsx
+++ web/src/features/setup/providers/SetupProvider.tsx
@@ -1,68 +1,109 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 
-import type { SetupOptions, SetupState } from '@databank/types';
-import { FormPageWrapper, useNotificationsStore } from '@douglasneuroinformatics/ui';
-import axios from 'axios';
-import { useTranslation } from 'react-i18next';
-import { match } from 'ts-pattern';
+// import type { SetupOptions, SetupState } from '@databank/types';
+// import { FormPageWrapper, useNotificationsStore } from '@douglasneuroinformatics/ui';
+// import axios from 'axios';
+// import { useTranslation } from 'react-i18next';
+// import { match } from 'ts-pattern';
 
-import { SetupForm } from '../components/SetupForm';
+// import { SetupForm } from '../components/SetupForm';
+
+// export const SetupProvider = ({ children }: { children: React.ReactNode }) => {
+//   const { t } = useTranslation();
+//   const notifications = useNotificationsStore();
+
+//   const [state, setState] = useState<SetupState>(() => {
+//     const savedSetup = window.localStorage.getItem('databankSetup');
+//     if (!savedSetup) {
+//       return { isSetup: false };
+//     }
+//     return JSON.parse(savedSetup) as SetupState;
+//   });
+
+//   const fetchSetupState = async () => {
+//     const response = await axios.get<SetupState>('/v1/setup');
+//     setState(response.data);
+//   };
+
+//   const handleSubmit = async (data: SetupOptions) => {
+//     try {
+//       await axios.post('/v1/setup', data);
+//       setState({ isSetup: true });
+//       notifications.addNotification({ type: 'success' });
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     window.localStorage.setItem('databankSetup', JSON.stringify(state));
+//     if (state.isSetup === null) {
+//       fetchSetupState().catch(console.error);
+//     } else if (!state.isSetup) {
+//       window.history.replaceState({}, '', '/setup');
+//     }
+//   }, [state]);
+
+//   return match(state)
+//     .with({ isSetup: null }, () => (
+//       <div>
+//         <p>Loading/Chargement...</p>
+//       </div>
+//     ))
+//     .with({ isSetup: false }, () => (
+//       <FormPageWrapper
+//         languageToggle={{
+//           dropdownDirection: 'up',
+//           options: ['en', 'fr']
+//         }}
+//         logo="/logo.png"
+//         title={t('setup')}
+//         widthMultiplier={1.5}
+//       >
+//         <SetupForm onSubmit={(data) => void handleSubmit(data)} />
+//       </FormPageWrapper>
+//     ))
+//     .with({ isSetup: true }, () => children)
+//     .exhaustive();
+// };
+import React, { useEffect } from 'react';
+
+import { useSetupState } from '@/hooks/useSetupState';
+
+import { useCreateSetupState } from '../hooks/useCreateSetupState';
+import { SetupLoadingPage } from '../pages/SetupLoadingPage';
+import { SetupPage } from '../pages/SetupPage';
 
 export const SetupProvider = ({ children }: { children: React.ReactNode }) => {
-  const { t } = useTranslation();
-  const notifications = useNotificationsStore();
-
-  const [state, setState] = useState<SetupState>(() => {
-    const savedSetup = window.localStorage.getItem('databankSetup');
-    if (!savedSetup) {
-      return { isSetup: false };
-    }
-    return JSON.parse(savedSetup) as SetupState;
-  });
-
-  const fetchSetupState = async () => {
-    const response = await axios.get<SetupState>('/v1/setup');
-    setState(response.data);
-  };
-
-  const handleSubmit = async (data: SetupOptions) => {
-    try {
-      await axios.post('/v1/setup', data);
-      setState({ isSetup: true });
-      notifications.addNotification({ type: 'success' });
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const setupStateQuery = useSetupState();
+  const createSetupStateMutation = useCreateSetupState();
 
   useEffect(() => {
-    window.localStorage.setItem('databankSetup', JSON.stringify(state));
-    if (state.isSetup === null) {
-      fetchSetupState().catch(console.error);
-    } else if (!state.isSetup) {
+    if (setupStateQuery.data?.isSetup === false) {
       window.history.replaceState({}, '', '/setup');
     }
-  }, [state]);
+  }, [setupStateQuery.data]);
 
-  return match(state)
-    .with({ isSetup: null }, () => (
-      <div>
-        <p>Loading/Chargement...</p>
-      </div>
-    ))
-    .with({ isSetup: false }, () => (
-      <FormPageWrapper
-        languageToggle={{
-          dropdownDirection: 'up',
-          options: ['en', 'fr']
-        }}
-        logo="/logo.png"
-        title={t('setup')}
-        widthMultiplier={1.5}
-      >
-        <SetupForm onSubmit={(data) => void handleSubmit(data)} />
-      </FormPageWrapper>
-    ))
-    .with({ isSetup: true }, () => children)
-    .exhaustive();
+  if (setupStateQuery.data?.isSetup !== false) {
+    return children;
+  } else if (createSetupStateMutation.isPending) {
+    return <SetupLoadingPage />;
+  }
+
+  return (
+    <SetupPage
+      onSubmit={({ dummySubjectCount, firstName, initDemo, lastName, password, username }) => {
+        createSetupStateMutation.mutate({
+          admin: {
+            firstName,
+            lastName,
+            password,
+            username
+          },
+          dummySubjectCount,
+          initDemo
+        });
+      }}
+    />
+  );
 };
---
+++ web/src/hooks/useSetupState.ts
@@ -0,0 +1,13 @@
+import { $SetupDto } from '@da';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+export function useSetupState() {
+  return useQuery({
+    queryFn: async () => {
+      const response = await axios.get('/v1/setup');
+      return $SetupState.parseAsync(response.data);
+    },
+    queryKey: ['setup-state']
+  });
+}
