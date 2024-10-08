0f23aac47f6fd0224f333c7bc403eb6e6400eac5
David Roper <davidroper321@gmail.com>
2024-10-08 10:49:37 -0400

    chore: remove unused config file

--- web/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-const configFunction = require('@douglasneuroinformatics/libui/tailwind/config');
-
-/** @type {import('tailwindcss').Config} */
-module.exports = configFunction({
-  content: ['./src/**/*.{js,ts,jsx,tsx}']
-});
