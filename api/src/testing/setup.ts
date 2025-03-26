8138a4264b0e2ea553b2e6358bf30fe2fdaa5560
joshunrau <contact@joshuaunrau.com>
2025-03-26 18:45:00 -0400

    load env before tests

--- api/src/testing/setup.ts
+++ api/src/testing/setup.ts
@@ -2,4 +2,6 @@ import * as path from 'node:path';
 
 import 'reflect-metadata';
 
+process.loadEnvFile(path.resolve(import.meta.dirname, '../../../.env'));
+
 process.env.LIBNEST_APP_BASE_DIR = path.resolve(import.meta.dirname, '../..');
