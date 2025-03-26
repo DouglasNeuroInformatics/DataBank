fdf7be3aa64295809305cfc54ef90f67f68e525c
joshunrau <contact@joshuaunrau.com>
2025-03-26 15:43:18 -0400

    test: update model name

--- api/src/setup/__tests__/setup.service.spec.ts
+++ api/src/setup/__tests__/setup.service.spec.ts
@@ -12,42 +12,42 @@ import { UsersService } from '@/users/users.service';
 import { SetupService } from '../setup.service';
 
 describe('SetupService', () => {
-  let setupModel: MockedInstance<Model<'Setup'>>;
+  let setupConfigModel: MockedInstance<Model<'SetupConfig'>>;
   let setupService: SetupService;
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       providers: [
-        MockFactory.createForModelToken(getModelToken('Setup')),
+        MockFactory.createForModelToken(getModelToken('SetupConfig')),
         MockFactory.createForService(DatasetsService),
         MockFactory.createForService(UsersService),
         SetupService
       ]
     }).compile();
-    setupModel = moduleRef.get(getModelToken('Setup'));
+    setupConfigModel = moduleRef.get(getModelToken('SetupConfig'));
     setupService = moduleRef.get(SetupService);
   });
 
   describe('getState', () => {
     it('should return that the app is not setup if there are no items in the setup collection', async () => {
-      setupModel.count.mockResolvedValueOnce(0);
+      setupConfigModel.count.mockResolvedValueOnce(0);
       await expect(setupService.getState()).resolves.toStrictEqual({ isSetup: false });
     });
     it('should return that the app is setup if there are one or more items in the setup collection', async () => {
-      setupModel.count.mockResolvedValueOnce(1);
+      setupConfigModel.count.mockResolvedValueOnce(1);
       await expect(setupService.getState()).resolves.toStrictEqual({ isSetup: true });
     });
   });
 
   describe('getVerificationStrategy', () => {
     it('should throw a ServiceUnavailableException if the app has not been setup', async () => {
-      setupModel.findFirst.mockResolvedValueOnce(null);
+      setupConfigModel.findFirst.mockResolvedValueOnce(null);
       await expect(setupService.getVerificationStrategy()).rejects.toMatchObject({
         status: HttpStatus.SERVICE_UNAVAILABLE
       });
     });
     it('should return the result from the db if it exists', async () => {
-      setupModel.findFirst.mockResolvedValueOnce({ verificationStrategy: 'STRATEGY' });
+      setupConfigModel.findFirst.mockResolvedValueOnce({ verificationStrategy: 'STRATEGY' });
       await expect(setupService.getVerificationStrategy()).resolves.toBe('STRATEGY');
     });
   });
