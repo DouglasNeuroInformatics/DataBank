8a621eb3403fe0ddf9074e19a0abbd58d9cf7149
joshunrau <contact@joshuaunrau.com>
2025-04-07 14:10:29 -0400

    add className prop

--- web/src/components/PageHeading.tsx
+++ web/src/components/PageHeading.tsx
@@ -1,8 +1,9 @@
 import { Heading, Separator } from '@douglasneuroinformatics/libui/components';
+import { cn } from '@douglasneuroinformatics/libui/utils';
 
-export const PageHeading: React.FC<{ children: string }> = ({ children }) => {
+export const PageHeading: React.FC<{ children: string; className?: string }> = ({ children, className }) => {
   return (
-    <div className="flex flex-col gap-8 py-8">
+    <div className={cn('flex flex-col gap-8 py-8', className)}>
       <Heading className="text-center" variant="h1">
         {children}
       </Heading>
