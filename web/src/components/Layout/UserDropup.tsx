db427176ecbac20932929ba78f31fab5c348113c
Weijie Tan <twj3454@hotmail.com>
2025-04-23 10:19:56 -0400

    fix: user preference route

--- web/src/components/Layout/UserDropup.tsx
+++ web/src/components/Layout/UserDropup.tsx
@@ -26,7 +26,7 @@ export const UserDropup = () => {
           <DropdownMenu.Item
             className="gap-2 hover:bg-slate-700 hover:text-slate-100 focus:bg-slate-700 focus:text-slate-100 focus:ring-0"
             onClick={() => {
-              navigate('/about');
+              navigate('/portal/user');
             }}
           >
             <SettingsIcon />
