499b426e53b7dcfcbb423a801ebdfdbc64761bd2
Weijie Tan <twj3454@hotmail.com>
2025-07-16 14:52:14 -0400

    feat: add landing page footer

---
+++ web/src/features/landing/components/LandingFooter.tsx
@@ -0,0 +1,34 @@
+const CURRENT_YEAR = new Date().getFullYear();
+
+export const LandingFooter = () => {
+  return (
+    <footer className="text-muted-foreground container py-3 text-sm">
+      <hr className="my-3 border-slate-200 dark:border-slate-700" />
+      <div className="flex items-center justify-center">
+        <div className="mb-1 flex flex-row flex-wrap font-medium lg:flex-nowrap">
+          <div className="flex w-1/2 items-center justify-center lg:w-auto">
+            <a
+              className="underline-offset-3 p-1 text-center hover:underline lg:mx-2"
+              href={'https://douglasneuroinformatics.ca'}
+              rel="noreferrer"
+              target="_blank"
+            >
+              Douglas Neuroinformatics
+            </a>
+          </div>
+          <div className="flex w-1/2 items-center justify-center lg:w-auto">
+            <a
+              className="underline-offset-3 p-1 text-center hover:underline lg:mx-2"
+              href={'https://github.com/DouglasNeuroInformatics/DataBank'}
+              rel="noreferrer"
+              target="_blank"
+            >
+              GitHub
+            </a>
+          </div>
+        </div>
+      </div>
+      <p className="text-center">&copy; {CURRENT_YEAR} Douglas Neuroinformatics Platform</p>
+    </footer>
+  );
+};
