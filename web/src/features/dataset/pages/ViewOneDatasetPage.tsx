bb12e79b9ab6a9587e69fa8d3016d2ea3eab7213
Weijie Tan <twj3454@hotmail.com>
2025-08-07 15:02:14 -0400

    feat: add translation for dataset sharable status

--- web/src/features/dataset/pages/ViewOneDatasetPage.tsx
+++ web/src/features/dataset/pages/ViewOneDatasetPage.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 import type { DatasetViewPagination, TabularDataset } from '@databank/core';
 import { capitalize } from '@douglasneuroinformatics/libjs';
-import { Button, Card, DropdownMenu, Heading } from '@douglasneuroinformatics/libui/components';
+import { Button, Card, DropdownMenu } from '@douglasneuroinformatics/libui/components';
 import {
   useDestructiveAction,
   useDownload,
@@ -130,15 +130,14 @@ const ViewOneDatasetPage = ({ isPublic }: ViewOneDatasetPageProps) => {
                 {t('manageDatasetManagers')}
               </Button>
 
-              <div className={`px-3 py-2 rounded-md text-sm font-medium ${
-                dataset.isReadyToShare 
-                  ? 'bg-green-100 text-green-800 border border-green-200' 
-                  : 'bg-orange-100 text-orange-800 border border-orange-200'
-              }`}>
-                {dataset.isReadyToShare 
-                  ? t('datasetReadyToShare') 
-                  : t('datasetNotReadyToShare')
-                }
+              <div
+                className={`rounded-md px-3 py-2 text-sm font-medium ${
+                  dataset.isReadyToShare
+                    ? 'border border-green-200 bg-green-100 text-green-800'
+                    : 'border border-orange-200 bg-orange-100 text-orange-800'
+                }`}
+              >
+                {dataset.isReadyToShare ? t('datasetReadyToShare') : t('datasetNotReadyToShare')}
               </div>
 
               <Button className="m-2" variant={'danger'} onClick={() => deleteDataset(params.datasetId!)}>
