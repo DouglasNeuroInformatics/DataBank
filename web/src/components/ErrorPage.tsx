69c8d3fe8b8e3df22e757dbc66b093fe492d91c0
Weijie Tan <twj3454@hotmail.com>
2025-08-20 12:35:49 -0400

    fix: add translations to error page

--- web/src/components/ErrorPage.tsx
+++ web/src/components/ErrorPage.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 
+import { useTranslation } from '@douglasneuroinformatics/libui/hooks';
+
 type ErrorPageProps = {
   error: {
     message: string;
@@ -11,13 +13,13 @@ export const ErrorPage = ({ error }: ErrorPageProps) => {
     console.error(error);
   }, [error]);
 
+  const { t } = useTranslation('common');
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-1 p-3 text-center">
-      <h1 className="text-muted-foreground text-sm font-semibold uppercase tracking-wide">Unexpected Error</h1>
-      <h3 className="text-3xl font-extrabold tracking-tight sm:text-4xl md:text-5xl">Something Went Wrong</h3>
-      <p className="text-muted-foreground mt-2 max-w-prose text-sm sm:text-base">
-        We apologize for the inconvenience. Please contact us for further assistance.
-      </p>
+      <h1 className="text-muted-foreground text-sm font-semibold uppercase tracking-wide">{t('unexpectedError')}</h1>
+      <h3 className="text-3xl font-extrabold tracking-tight sm:text-4xl md:text-5xl">{t('somethingWentWrong')}</h3>
+      <p className="text-muted-foreground mt-2 max-w-prose text-sm sm:text-base">{t('apologizeForInconvenience')}</p>
       <div className="mt-6">
         <button
           className="text-sky-800 underline-offset-4 hover:text-sky-700 hover:underline dark:text-sky-200 dark:hover:text-sky-300"
@@ -26,7 +28,8 @@ export const ErrorPage = ({ error }: ErrorPageProps) => {
             window.location.assign(window.location.origin);
           }}
         >
-          Reload Page<span aria-hidden="true"> &rarr;</span>
+          {t('reloadPage')}
+          <span aria-hidden="true"> &rarr;</span>
         </button>
       </div>
     </div>
