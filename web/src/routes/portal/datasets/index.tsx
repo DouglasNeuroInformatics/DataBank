0015db222672c0030ea9d31b5fd4d9e958e4acdc
Weijie Tan <twj3454@hotmail.com>
2025-09-17 15:44:36 -0400

    feat: move data fetching to view datasets router, ensureQueryData, pass data to view dataset page

--- web/src/routes/portal/datasets/index.tsx
+++ web/src/routes/portal/datasets/index.tsx
@@ -1,7 +1,25 @@
+import { $DatasetInfo } from '@databank/core';
+import { keepPreviousData, queryOptions, useSuspenseQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
+import axios from 'axios';
+import z from 'zod/v4';
 
 import { ViewDatasetsPage } from '@/features/dataset/pages/ViewDatasetsPage';
+import { queryClient } from '@/services/react-query';
+
+const viewDatasetsQueryOptions = queryOptions({
+  placeholderData: keepPreviousData,
+  queryFn: async () => {
+    const response = await axios.get<$DatasetInfo[]>('/v1/datasets');
+    return z.array($DatasetInfo).parse(response.data);
+  },
+  queryKey: ['datasets-info']
+});
 
 export const Route = createFileRoute('/portal/datasets/')({
-  component: () => <ViewDatasetsPage isPublic={false} />
+  component: () => {
+    const { data } = useSuspenseQuery(viewDatasetsQueryOptions);
+    return <ViewDatasetsPage datasetsInfoArray={data} isPublic={false} />;
+  },
+  loader: () => queryClient.ensureQueryData(viewDatasetsQueryOptions)
 });
