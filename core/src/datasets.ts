a8f342fce1e0cb36a228358dbf5aa32d2ee36f2d
Weijie Tan <twj3454@hotmail.com>
2025-09-26 10:38:12 -0400

    fix: add more restriction to the dataset view pagination schema in core

--- core/src/datasets.ts
+++ core/src/datasets.ts
@@ -4,8 +4,8 @@ import { $PermissionLevel, $TabularColumnSummary } from './columns';
 import { $DatasetLicenses } from './licenses';
 
 const $DatasetViewPagination = z.object({
-  currentPage: z.number(),
-  itemsPerPage: z.number()
+  currentPage: z.number().int().gte(1),
+  itemsPerPage: z.number().int().gte(1)
 });
 type $DatasetViewPagination = z.infer<typeof $DatasetViewPagination>;
 
