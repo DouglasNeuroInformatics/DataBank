23eac1d69340961c9f90ef5b7349cc43ed22684b
Weijie Tan <twj3454@hotmail.com>
2025-10-01 15:53:23 -0400

    refactor: extract public dataset query logic into a function

--- web/src/routes/public/dataset.$datasetId.tsx
+++ web/src/routes/public/dataset.$datasetId.tsx
@@ -14,55 +14,44 @@ const $ViewOneDatasetPageSearchParams = z.object({
   rowPagination: $DatasetViewPagination.default({ currentPage: 1, itemsPerPage: 10 })
 });
 
+const getPublicDatasetQueryOptions = (
+  datasetId: string,
+  columnPagination: $DatasetViewPagination,
+  rowPagination: $DatasetViewPagination
+) => {
+  const dataQueryUrl = `/v1/datasets/public/${datasetId}`;
+  return queryOptions({
+    queryFn: async () => {
+      const response = await axios.post<$TabularDataset>(dataQueryUrl, {
+        columnPagination,
+        rowPagination
+      });
+      return $TabularDataset.parse(response.data);
+    },
+    queryKey: [
+      'dataset-query',
+      datasetId,
+      columnPagination.currentPage,
+      columnPagination.itemsPerPage,
+      rowPagination.currentPage,
+      rowPagination.itemsPerPage
+    ]
+  });
+};
+
 export const Route = createFileRoute('/public/dataset/$datasetId')({
   validateSearch: zodValidator($ViewOneDatasetPageSearchParams),
   loaderDeps: ({ search: { columnPagination, rowPagination } }) => ({ columnPagination, rowPagination }),
   loader: async ({ deps: { columnPagination, rowPagination }, params }) => {
-    const dataQueryUrl = `/v1/datasets/public/${params.datasetId}`;
-
-    const viewOnePublicDatasetOptions = queryOptions({
-      queryFn: async () => {
-        const response = await axios.post<$TabularDataset>(dataQueryUrl, {
-          columnPagination,
-          rowPagination
-        });
-        return $TabularDataset.parse(response.data);
-      },
-      queryKey: [
-        'dataset-query',
-        params.datasetId,
-        columnPagination.currentPage,
-        columnPagination.itemsPerPage,
-        rowPagination.currentPage,
-        rowPagination.itemsPerPage
-      ]
-    });
-
+    const viewOnePublicDatasetOptions = getPublicDatasetQueryOptions(params.datasetId, columnPagination, rowPagination);
     await queryClient.ensureQueryData(viewOnePublicDatasetOptions);
   },
   component: () => {
     const { columnPagination, rowPagination } = useSearch({ from: '/public/dataset/$datasetId' });
     const params = useParams({ from: '/public/dataset/$datasetId' });
-    const dataQueryUrl = `/v1/datasets/public/${params.datasetId}`;
     const downloadDataUrl = `/v1/datasets/public/download-data/`;
     const downloadMetaDataUrl = `/v1/datasets/public/download-metadata/`;
-    const viewOnePublicDatasetOptions = queryOptions({
-      queryFn: async () => {
-        const response = await axios.post<$TabularDataset>(dataQueryUrl, {
-          columnPagination,
-          rowPagination
-        });
-        return $TabularDataset.parse(response.data);
-      },
-      queryKey: [
-        'dataset-query',
-        params.datasetId,
-        columnPagination.currentPage,
-        columnPagination.itemsPerPage,
-        rowPagination.currentPage,
-        rowPagination.itemsPerPage
-      ]
-    });
+    const viewOnePublicDatasetOptions = getPublicDatasetQueryOptions(params.datasetId, columnPagination, rowPagination);
     const datasetQuery = useSuspenseQuery(viewOnePublicDatasetOptions);
     const dataset = datasetQuery.data;
 
