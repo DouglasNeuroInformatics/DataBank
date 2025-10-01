cc7d98e1a54efb6ec62e0799cee5dbf125939a34
Weijie Tan <twj3454@hotmail.com>
2025-10-01 12:47:25 -0400

    feat: in public and login dataset router, add loader and change use suspense query in the component

--- web/src/routes/portal/datasets/$datasetId.tsx
+++ web/src/routes/portal/datasets/$datasetId.tsx
@@ -1,7 +1,65 @@
-import { createFileRoute } from '@tanstack/react-router';
+/* eslint-disable perfectionist/sort-objects */
+import { $DatasetViewPagination, $TabularDataset } from '@databank/core';
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
+import { createFileRoute, useParams, useSearch } from '@tanstack/react-router';
+import { zodValidator } from '@tanstack/zod-adapter';
+import axios from 'axios';
+import z from 'zod/v4';
 
 import { ViewOneDatasetPage } from '@/features/dataset/pages/ViewOneDatasetPage';
+import { queryClient } from '@/services/react-query';
+
+const $ViewOneDatasetPageSearchParams = z.object({
+  columnPagination: $DatasetViewPagination.default({ currentPage: 1, itemsPerPage: 10 }),
+  rowPagination: $DatasetViewPagination.default({ currentPage: 1, itemsPerPage: 10 })
+});
 
 export const Route = createFileRoute('/portal/datasets/$datasetId')({
-  component: () => <ViewOneDatasetPage isPublic={false} />
+  validateSearch: zodValidator($ViewOneDatasetPageSearchParams),
+  loaderDeps: ({ search: { columnPagination, rowPagination } }) => ({ columnPagination, rowPagination }),
+  loader: async ({ deps: { columnPagination, rowPagination }, params }) => {
+    const dataQueryUrl = `/v1/datasets/${params.datasetId}`;
+
+    const viewOneDatasetOptions = queryOptions({
+      queryFn: async () => {
+        const response = await axios.post<$TabularDataset>(dataQueryUrl, {
+          columnPagination,
+          rowPagination
+        });
+        return $TabularDataset.parse(response.data);
+      },
+      queryKey: ['dataset-query', params.datasetId, columnPagination, rowPagination]
+    });
+
+    await queryClient.ensureQueryData(viewOneDatasetOptions);
+  },
+  component: () => {
+    const { columnPagination, rowPagination } = useSearch({ from: '/portal/datasets/$datasetId' });
+    const params = useParams({ from: '/portal/datasets/$datasetId' });
+    const dataQueryUrl = `/v1/datasets/${params.datasetId}`;
+    const downloadDataUrl = `/v1/datasets/download-data/`;
+    const downloadMetaDataUrl = `/v1/datasets/download-metadata/`;
+    const viewOneDatasetOptions = queryOptions({
+      queryFn: async () => {
+        const response = await axios.post<$TabularDataset>(dataQueryUrl, {
+          columnPagination,
+          rowPagination
+        });
+        return $TabularDataset.parse(response.data);
+      },
+      queryKey: ['dataset-query', params.datasetId, columnPagination, rowPagination]
+    });
+    const datasetQuery = useSuspenseQuery(viewOneDatasetOptions);
+    const dataset = datasetQuery.data;
+
+    return (
+      <ViewOneDatasetPage
+        columnPagination={columnPagination}
+        dataset={dataset}
+        downloadDataUrl={downloadDataUrl}
+        downloadMetaDataUrl={downloadMetaDataUrl}
+        rowPagination={rowPagination}
+      />
+    );
+  }
 });
--- web/src/routes/public/dataset.$datasetId.tsx
+++ web/src/routes/public/dataset.$datasetId.tsx
@@ -1,7 +1,65 @@
-import { createFileRoute } from '@tanstack/react-router';
+/* eslint-disable perfectionist/sort-objects */
+import { $DatasetViewPagination, $TabularDataset } from '@databank/core';
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
+import { createFileRoute, useParams, useSearch } from '@tanstack/react-router';
+import { zodValidator } from '@tanstack/zod-adapter';
+import axios from 'axios';
+import z from 'zod/v4';
 
 import { ViewOneDatasetPage } from '@/features/dataset/pages/ViewOneDatasetPage';
+import { queryClient } from '@/services/react-query';
+
+const $ViewOneDatasetPageSearchParams = z.object({
+  columnPagination: $DatasetViewPagination.default({ currentPage: 1, itemsPerPage: 10 }),
+  rowPagination: $DatasetViewPagination.default({ currentPage: 1, itemsPerPage: 10 })
+});
 
 export const Route = createFileRoute('/public/dataset/$datasetId')({
-  component: () => <ViewOneDatasetPage isPublic={true} />
+  validateSearch: zodValidator($ViewOneDatasetPageSearchParams),
+  loaderDeps: ({ search: { columnPagination, rowPagination } }) => ({ columnPagination, rowPagination }),
+  loader: async ({ deps: { columnPagination, rowPagination }, params }) => {
+    const dataQueryUrl = `/v1/datasets/public/${params.datasetId}`;
+
+    const viewOnePublicDatasetOptions = queryOptions({
+      queryFn: async () => {
+        const response = await axios.post<$TabularDataset>(dataQueryUrl, {
+          columnPagination,
+          rowPagination
+        });
+        return $TabularDataset.parse(response.data);
+      },
+      queryKey: ['dataset-query', params.datasetId, columnPagination, rowPagination]
+    });
+
+    await queryClient.ensureQueryData(viewOnePublicDatasetOptions);
+  },
+  component: () => {
+    const { columnPagination, rowPagination } = useSearch({ from: '/public/dataset/$datasetId' });
+    const params = useParams({ from: '/public/dataset/$datasetId' });
+    const dataQueryUrl = `/v1/datasets/public/${params.datasetId}`;
+    const downloadDataUrl = `/v1/datasets/public/download-data/`;
+    const downloadMetaDataUrl = `/v1/datasets/public/download-metadata/`;
+    const viewOnePublicDatasetOptions = queryOptions({
+      queryFn: async () => {
+        const response = await axios.post<$TabularDataset>(dataQueryUrl, {
+          columnPagination,
+          rowPagination
+        });
+        return $TabularDataset.parse(response.data);
+      },
+      queryKey: ['dataset-query', params.datasetId, columnPagination, rowPagination]
+    });
+    const datasetQuery = useSuspenseQuery(viewOnePublicDatasetOptions);
+    const dataset = datasetQuery.data;
+
+    return (
+      <ViewOneDatasetPage
+        columnPagination={columnPagination}
+        dataset={dataset}
+        downloadDataUrl={downloadDataUrl}
+        downloadMetaDataUrl={downloadMetaDataUrl}
+        rowPagination={rowPagination}
+      />
+    );
+  }
 });
