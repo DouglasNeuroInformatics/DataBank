b5116c274507636a706363d963a6921411fc3068
joshunrau <contact@joshuaunrau.com>
2026-07-14 10:57:18 -0400

    add centered option to page heading

--- web/src/components/PageHeading.tsx
+++ web/src/components/PageHeading.tsx
@@ -3,15 +3,18 @@ import { cn } from '@douglasneuroinformatics/libui/utils';
 
 export const PageHeading: React.FC<{
   actions?: React.ReactNode;
+  centered?: boolean;
   children: string;
   className?: string;
   description?: null | string;
-}> = ({ actions, children, className, description }) => {
+}> = ({ actions, centered, children, className, description }) => {
   return (
     <div className={cn('flex flex-col gap-2 py-6', className)}>
       <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
-        <div className="min-w-0">
-          <Heading variant="h2">{children}</Heading>
+        <div className="w-full min-w-0">
+          <Heading className={cn(centered && 'text-center')} variant="h2">
+            {children}
+          </Heading>
           {description && <p className="text-muted-foreground mt-1 text-sm">{description}</p>}
         </div>
         {actions && <div className="flex shrink-0 items-center gap-2">{actions}</div>}
